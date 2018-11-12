0bfbd859dab06138b8acdd1e4cac8711c82ad6f6
penglei <1317821900@qq.com>
2018-11-12 20:51:44 +0800

    1.完善数据圈详情页。 2.新增数据圈子评论页面。

--- src/pages/knowladge/itemDetail/main.js
+++ src/pages/knowladge/itemDetail/main.js
@@ -2,8 +2,6 @@ Page({
     data: {
         show_share: false,  // 分享
         show_setting: false, // 设置
-        show_publish: false, // 发表
-        show_filter: false // 筛选
     },
 
 
@@ -15,16 +13,6 @@ Page({
     changeShare() {
         this.setData({
             show_share: !this.data.show_share
-        })
-    },
-    changePublish() {
-        this.setData({
-            show_publish: !this.data.show_publish
-        })
-    },
-    changeFilter() {
-        this.setData({
-            show_filter: !this.data.show_filter
         })
     }
 })
