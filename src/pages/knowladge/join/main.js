593ae61c8393e96d1bebe46e4d8df03c6a6562ae
penglei <1317821900@qq.com>
2018-11-23 09:43:36 +0800

    update

--- src/pages/knowladge/join/main.js
+++ src/pages/knowladge/join/main.js
@@ -1,10 +1,26 @@
+import {config,getUrl,pageLogin,sandBox,cookieStorage} from '../../../lib/myapp.js';
+
 Page({
     data: {
         descAll: false,
-        guestAll: false
+        guestAll: false,
+        id: '',
+        detail: '',
+        init: false
     },
-
-
+    onLoad(e) {
+      if (e.id) {
+          this.setData({
+              id: e.id
+          })
+          this.getDetail(e.id)
+      } else {
+          wx.showModal({
+              content: '参数错误',
+              showCancel: false
+          })
+      }
+    },
     changeDesc() {
         this.setData({
             descAll: !this.data.descAll
@@ -13,6 +29,98 @@ Page({
     changeGuest() {
         this.setData({
             guestAll: !this.data.guestAll
+        })
+    },
+    // 更新用户信息
+    bindgetuserinfo(e) {
+        if (e.detail.userInfo) {
+            this.updateUserInfo(e.detail.userInfo)
+        }
+    },
+    updateUserInfo(e){
+        wx.showLoading({
+            title: '更新中',
+            mask: true
+        })
+        var token = cookieStorage.get('user_token')
+        sandBox.post({
+            api: 'api/users/update/info',
+            header:{
+                Authorization: token
+            },
+            data:{
+                nick_name:e.nickName,
+                sex:e.gender == 1 ? '男' : '女',
+                avatar:e.avatarUrl,
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    wx.showToast({
+                        title:'修改成功',
+                        duration: 1500,
+                        success:()=>{
+                            setTimeout(()=>{
+
+                            },1500);
+                        }
+                    })
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "更新失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"更新失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    // 获取圈子详情
+    getDetail(id) {
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        })
+        var token = cookieStorage.get('user_token')
+        sandBox.get({
+            api: 'api/coterie',
+            header:{
+                Authorization: token
+            },
+            data:{
+                id: id
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    this.setData({
+                        detail: res.data,
+                        init: true
+                    })
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
         })
     }
 })
