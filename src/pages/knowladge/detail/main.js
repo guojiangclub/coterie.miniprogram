b72da8e53091496eabf35a63494430c0f49a1dc2
qianzaizai <xiangqian157@foxmail.com>
2018-11-29 18:08:46 +0800

    update 评论

--- src/pages/knowladge/detail/main.js
+++ src/pages/knowladge/detail/main.js
@@ -1,4 +1,5 @@
 import {config,getUrl,pageLogin,sandBox,cookieStorage} from '../../../lib/myapp.js';
+var a;
 Page({
     data: {
         show_share: false,  // 分享
@@ -17,7 +18,12 @@ Page({
         tagpage:0,
         tagMore:true,
         type:'',
-        tagname:''
+        tagname:'',
+        tagtitle:'',
+        selectItem:{
+            is_stick:'',
+            is_recommend:''
+        },//选中的item
     },
     onLoad(e){
       this.setData({
@@ -25,6 +31,10 @@ Page({
       })
     },
     onShow(){
+        if(a){
+            a = false;
+            return
+        }
         this.getDetail(this.data.id);
         this.getStick(this.data.id);
         this.getContnetList(this.data.id,1,this.data.type,this.data.tagname);
@@ -54,7 +64,16 @@ Page({
             });
         }
     },
-    changeSetting() {
+    changeSetting(e) {
+        this.setData({
+            selectItem: e.currentTarget.dataset.item
+        }, () => {
+            this.setData({
+                show_setting: !this.data.show_setting
+            })
+        })
+    },
+    changeSet(){
         this.setData({
             show_setting: !this.data.show_setting
         })
@@ -266,6 +285,12 @@ Page({
         } else {
             tagname = '' ;
         }
+        if(e.currentTarget.dataset.title){
+            var title = e.currentTarget.dataset.title;
+            this.setData({
+                tagtitle:title
+            })
+        }
         this.setData({
             tagname:tagname,
             type:type
@@ -274,5 +299,205 @@ Page({
         this.setData({
             show_filter:false
         })
+    },
+    //圈主设置置顶动态或者取消动态
+    postSetStick(content_id,coterie_id,type){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        })
+        var token = cookieStorage.get('user_token')
+        sandBox.post({
+            api: 'api/content/stick',
+            header:{
+                Authorization: token
+            },
+            data:{
+                content_id: content_id,
+                coterie_id:coterie_id,
+                type:type
+
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    this.setData({
+                        show_setting:false
+                    });
+                    this.getStick(this.data.id);//重新请求置顶状态
+                    this.getContnetList(this.data.id,1,this.data.type,this.data.tagname);
+                    if(type == 1){
+                        wx.showToast({
+                            title:'置顶成功',
+                            icon:'success'
+                        })
+                    } else {
+                        wx.showToast({
+                            title:'取消置顶',
+                            icon:'success'
+                        })
+                    }
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    //圈主设置为置顶状态
+    setStick(){
+        //type = 1 为置顶
+        var content_id = this.data.selectItem.id;
+        var coterie_id = this.data.selectItem.coterie_id;
+        this.postSetStick(content_id,coterie_id,1);
+    },
+    cancleStick(){
+        //type = 0 为取消置顶
+        var content_id = this.data.selectItem.id;
+        var coterie_id = this.data.selectItem.coterie_id;
+        this.postSetStick(content_id,coterie_id,0);
+    },
+    //圈主或者嘉宾推荐精华或者取消精华
+    postSetRecommend(content_id,coterie_id,type){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        })
+        var token = cookieStorage.get('user_token');
+        sandBox.post({
+            api: 'api/content/recommended',
+            header:{
+                Authorization: token
+            },
+            data:{
+                content_id: content_id,
+                coterie_id:coterie_id,
+                type:type
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    this.setData({
+                        show_setting:false
+                    });
+                    this.getContnetList(this.data.id,1,this.data.type,this.data.tagname);
+                    if(type == 1){
+                        wx.showToast({
+                            title:'标记成功',
+                            icon:'success'
+                        })
+                    } else {
+                        wx.showToast({
+                            title:'取消成功',
+                            icon:'success'
+                        })
+                    }
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    //圈主设置为推荐精华
+    setRecommend(){
+        //type = 1 为置顶
+        var content_id = this.data.selectItem.id;
+        var coterie_id = this.data.selectItem.coterie_id;
+        this.postSetRecommend(content_id,coterie_id,1);
+    },
+    cancleRecommend(){
+        //type = 0 为取消置顶
+        var content_id = this.data.selectItem.id;
+        var coterie_id = this.data.selectItem.coterie_id;
+        this.postSetRecommend(content_id,coterie_id,0);
+    },
+    //请求点赞接口
+    postPraise(content_id,idx,index){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        })
+        var token = cookieStorage.get('user_token');
+        sandBox.post({
+            api: 'api/content/praise/store',
+            header:{
+                Authorization: token
+            },
+            data:{
+                content_id: content_id
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                   //
+                    this.setData({
+                        [`contentList[${idx}][${index}].is_praise_user`]:1,
+                        [`contentList[${idx}][${index}].praise_count`]:this.data.contentList[idx][index].praise_count+1
+                    })
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    //点赞
+    givePraise(e){
+        var idx = e.currentTarget.dataset.idx;
+        var index = e.currentTarget.dataset.index;
+        var content_id = e.currentTarget.dataset.id;
+        this.postPraise(content_id,idx,index);
+    },
+    //跳到数据圈动态详情页
+    jumpItemDetail(e){
+        var content_id =  e.currentTarget.dataset.id;
+        wx.navigateTo({
+            url:'/pages/knowladge/itemDetail/main?id='+this.data.id + '&content_id='+content_id
+        })
+
+    },
+    //预览图
+    preImage(e){
+        a = true;
+        var url = e.currentTarget.dataset.url;
+        var urlList  = e.currentTarget.dataset.urllist;
+        wx.previewImage({
+            current:url,
+            urls:urlList
+        })
     }
-})
+    })
--- src/pages/knowladge/itemDetail/main.js
+++ src/pages/knowladge/itemDetail/main.js
@@ -1,18 +1,257 @@
+import {config,getUrl,pageLogin,sandBox,cookieStorage} from '../../../lib/myapp.js';
+var a;
 Page({
     data: {
         show_share: false,  // 分享
         show_setting: false, // 设置
+        show_comment:false,//评论
+        id:'',//圈子id
+        content_id:'',//圈子item  id
+        item:{},
+        commentValue:'',
+        commentList:[],//评论列表
+        page:0,
+        hasMore:true,
+        commentId:''
     },
-
-
+    onLoad(e){
+        this.setData({
+            id:e.id,
+            content_id:e.content_id
+        })
+    },
+    onShow(){
+        if(a){
+            a = false;
+            return
+        }
+        this.postContent(this.data.id,this.data.content_id);
+        this.getcomment(this.data.content_id,1);
+    },
     changeSetting() {
         this.setData({
             show_setting: !this.data.show_setting
         })
     },
+    //显示评论模块
+    changeComment(e){
+        this.setData({
+            show_comment: !this.data.show_comment
+        })
+        if(e.currentTarget.dataset.id){
+            this.setData({
+                commentId:e.currentTarget.dataset.id
+            })
+        }
+    },
     changeShare() {
         this.setData({
             show_share: !this.data.show_share
         })
-    }
+    },
+    //预览图
+    preImage(e){
+        a = true;
+        var url = e.currentTarget.dataset.url;
+        var urlList  = e.currentTarget.dataset.urllist;
+        wx.previewImage({
+            current:url,
+            urls:urlList
+        })
+    },
+    //请求数据圈动态详情页接口
+    postContent(coterie_id,content_id){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        })
+        var token = cookieStorage.get('user_token');
+        sandBox.get({
+            api: 'api/content',
+            header:{
+                Authorization: token
+            },
+            data:{
+                coterie_id:coterie_id,
+                content_id:content_id
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    this.setData({
+                        item:res.data
+                    })
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    //请求评论接口
+    postComment(content_id,content){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        });
+        var token = cookieStorage.get('user_token');
+        sandBox.post({
+            api: 'api/comment/store',
+            header:{
+                Authorization: token
+            },
+            data:{
+                content:content,
+                content_id:content_id
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    this.setData({
+                        commentValue:''
+                    })
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    //获取评论内容
+    getComment(e){
+        this.setData({
+            commentValue:e.detail.value
+        })
+    },
+    //点击发送按钮
+    sendComment(){
+        if(this.data.commentValue){
+            if(this.data.commentId){
+                this.replyComment(this.data.content_id,this.data.commentValue,this.data.commentId);
+            }else {
+                this.postComment(this.data.content_id,this.data.commentValue);
+            }
+            this.setData({
+                show_comment:!this.data.show_comment
+            })
+        }
+    },
+    //点击幕布，清空数据
+    clearData(){
+      this.setData({
+          show_comment:!this.data.show_comment,
+          commentValue:'',
+          commentId:''
+      })
+    },
+    //请求评论列表
+    getcomment(content_id,page){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        })
+        var token = cookieStorage.get('user_token');
+        sandBox.get({
+            api: 'api/comment',
+            header:{
+                Authorization: token
+            },
+            data:{
+                content_id:content_id,
+                page:page
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    //分页数据
+                    var pages = res.meta.pagination;
+                    var current_page = pages.current_page;
+                    var total_pages = pages.total_pages;
+                    this.setData({
+                        [`commentList[${page-1}]`]:res.data,
+                        page:current_page,
+                        hasMore:current_page < total_pages
+                    })
+
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    //请求回复评论接口
+    replyComment(content_id,content,comment_id){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        });
+        var token = cookieStorage.get('user_token');
+        sandBox.post({
+            api: 'api/reply/store',
+            header:{
+                Authorization: token
+            },
+            data:{
+                content:content,
+                content_id:content_id,
+                comment_id:comment_id
+    },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    this.setData({
+                        commentValue:''
+                    })
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
 })
