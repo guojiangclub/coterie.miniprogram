dd5b1a8ad96aebd447ccee7336174648e9f70e53
qianzaizai <xiangqian157@foxmail.com>
2018-11-30 17:59:25 +0800

    update

--- src/pages/knowladge/itemDetail/main.js
+++ src/pages/knowladge/itemDetail/main.js
@@ -7,12 +7,14 @@ Page({
         show_comment:false,//评论
         id:'',//圈子id
         content_id:'',//圈子item  id
-        item:{},
+        itemdetail:{},
         commentValue:'',
         commentList:[],//评论列表
         page:0,
         hasMore:true,
-        commentId:''
+        commentId:'',
+        to_meta:{},
+        replyType:''
     },
     onLoad(e){
         this.setData({
@@ -33,6 +35,18 @@ Page({
             show_setting: !this.data.show_setting
         })
     },
+    //剪贴板的内容
+    clipBoard(e){
+        var link = e.currentTarget.dataset.link;
+        wx.getClipboardData({
+            data:link,
+            success:res=>{
+                wx.showToast({
+                    title:'复制链接成功'
+                })
+            }
+        })
+    },
     //显示评论模块
     changeComment(e){
         this.setData({
@@ -42,6 +56,25 @@ Page({
             this.setData({
                 commentId:e.currentTarget.dataset.id
             })
+        }
+        if(e.currentTarget.dataset.type){
+            if (e.currentTarget.dataset.type == 'one'){
+                //一级不需要传nick-name 跟user-id
+                this.setData({
+                    replyType:e.currentTarget.dataset.type
+                })
+            } else if(e.currentTarget.dataset.type == 'second'){
+                //二级回复需要nick_name 跟 user-id
+                var nick_name = e.currentTarget.dataset.nickname;
+                var user_id = e.currentTarget.dataset.userid;
+                this.setData({
+                    to_meta:{
+                        id:user_id,
+                        nick_name:nick_name
+                    },
+                    replyType:e.currentTarget.dataset.type
+                })
+            }
         }
     },
     changeShare() {
@@ -80,7 +113,7 @@ Page({
                 res = res.data;
                 if (res.status) {
                     this.setData({
-                        item:res.data
+                        itemdetail:res.data
                     })
                 } else {
                     wx.showModal({
@@ -122,6 +155,9 @@ Page({
                     this.setData({
                         commentValue:''
                     })
+                    wx.showToast({
+                        title:'评论成功'
+                    })
                 } else {
                     wx.showModal({
                         content:res.message ||  "请求失败",
@@ -149,7 +185,11 @@ Page({
     sendComment(){
         if(this.data.commentValue){
             if(this.data.commentId){
-                this.replyComment(this.data.content_id,this.data.commentValue,this.data.commentId);
+                if(this.data.replyType == 'one'){
+                    this.replyComment(this.data.content_id,this.data.commentValue,this.data.commentId,'');
+                } else if(this.data.replyType == 'second'){
+                    this.replyComment(this.data.content_id,this.data.commentValue,this.data.commentId,this.data.to_meta);
+                }
             }else {
                 this.postComment(this.data.content_id,this.data.commentValue);
             }
@@ -163,7 +203,9 @@ Page({
       this.setData({
           show_comment:!this.data.show_comment,
           commentValue:'',
-          commentId:''
+          commentId:'',
+          to_meta:{},
+          replyType:''
       })
     },
     //请求评论列表
@@ -214,7 +256,7 @@ Page({
         })
     },
     //请求回复评论接口
-    replyComment(content_id,content,comment_id){
+    replyComment(content_id,content,comment_id,to_meta){
         wx.showLoading({
             title: '加载中',
             mask: true
@@ -228,7 +270,8 @@ Page({
             data:{
                 content:content,
                 content_id:content_id,
-                comment_id:comment_id
+                comment_id:comment_id,
+                to_meta:to_meta || ''
     },
         }).then(res =>{
             if(res.statusCode==200){
@@ -236,6 +279,9 @@ Page({
                 if (res.status) {
                     this.setData({
                         commentValue:''
+                    })
+                    wx.showToast({
+                        title:'回复评论成功'
                     })
                 } else {
                     wx.showModal({
@@ -254,4 +300,109 @@ Page({
             }
         })
     },
+    //删除评论
+    postdDelete(comment_id,idx,index){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        });
+        var token = cookieStorage.get('user_token');
+        sandBox.post({
+            api: 'api/comment/delete',
+            header:{
+                Authorization: token
+            },
+            data:{
+                comment_id:comment_id
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    var list = this.data.commentList;
+                    list[idx].splice(index,1);
+                    this.setData({
+                        commentList:list,
+                        'itemdetail.comment_count':this.data.itemdetail.comment_count+1
+                    })
+                    wx.showToast({
+                        title:'删除成功'
+                    })
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    //点击删除按钮
+    deleteComment(e){
+        var idx = e.currentTarget.dataset.idx;
+        var index = e.currentTarget.dataset.index;
+        var id = e.currentTarget.dataset.id;
+        this.postdDelete(id,idx,index);
+    },
+    //请求内容点赞接口
+    postPraise(content_id){
+        wx.showLoading({
+            title: '加载中',
+            mask: true
+        })
+        var token = cookieStorage.get('user_token');
+        sandBox.post({
+            api: 'api/content/praise/store',
+            header:{
+                Authorization: token
+            },
+            data:{
+                content_id: content_id
+            },
+        }).then(res =>{
+            if(res.statusCode==200){
+                res = res.data;
+                if (res.status) {
+                    var  list = this.data.itemdetail.praise;
+                    list.push(res.data);
+                    this.setData({
+                        'itemdetail.is_praise_user':1,
+                        'itemdetail.praise_count':this.data.itemdetail.praise_count+1,
+                        'itemdetail.praise':list
+                    })
+                } else {
+                    wx.showModal({
+                        content:res.message ||  "请求失败",
+                        showCancel: false
+                    });
+                }
+                wx.hideLoading();
+            }
+            else{
+                wx.showModal({
+                    content:"请求失败",
+                    showCancel: false
+                });
+                wx.hideLoading();
+            }
+        })
+    },
+    //点击给内容点赞
+    praiseStore(){
+        if (this.data.itemdetail.is_praise_user == 1){
+            return
+        } else {
+            this.postPraise(this.data.content_id)
+        }
+    },
+    //请求评论点赞接口
+
 })
